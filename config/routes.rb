8f68310c088258e6fddd5f8d178b399fde5607dc
Trevor Seitz <trevinla@gmail.com>
2018-06-26 17:41:17 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :new]
+  resources :posts, only: [:index, :new, :create]
+  resources :students, only: [:index, :new, :create]
+
+  # get "posts/new", to: "posts#new", as: :posts
 end
